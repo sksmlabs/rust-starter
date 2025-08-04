2fc75889408086750ab1f3e3239f382326262105
Saksham Jain <sksmlabs@Sakshams-MacBook-Pro.local>
2025-08-04 13:52:06 +0530

    tutorial: compound

--- src/bin/compound.rs
+++ src/bin/compound.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // Data type: Tuple
     let tup: (u16, f32, char) = (500, 6.4, 'c');
@@ -8,4 +10,32 @@ fn main() {
 
     let five_hundred = tup.0;
     println!("five hundred: {}", five_hundred);
+
+    // Data type: Array
+    let arr: [u32; 5] = [1, 2, 3, 4, 5];
+    println!("arr: {:?}", arr);
+
+    let arr_1 = arr[0];
+    println!("arr_1: {}", arr_1);
+
+    let arr = ["hello"; 5];
+    println!("arr: {:?}", arr);
+
+    // Invalid array element
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = arr[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
