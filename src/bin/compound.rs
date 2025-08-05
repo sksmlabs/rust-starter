ae82fe87a3890d207f8b825ff273b4cd75b82e66
Saksham Jain <sksmlabs@Sakshams-MacBook-Pro.local>
2025-08-05 13:59:13 +0530

    tutorial: compound

--- src/bin/compound.rs
+++ src/bin/compound.rs
@@ -1,3 +1,5 @@
+// Tutorial Link: https://www.youtube.com/watch?v=WbZdyDN_SE4
+
 use std::io;
 
 fn main() {
@@ -5,11 +7,26 @@ fn main() {
     let tup: (u16, f32, char) = (500, 6.4, 'c');
     println!("tuple: {:?}", tup); 
     
+    // Destructure
     let (x, y, z) = tup;
     println!("x: {} y: {} z: {}", x, y, z);
 
+    // Ignore with _
+    let (_, b, _) = tup;
+    println!("b: {}", b);
+
+    // Empty tuple - unit type
+    let t = ();
+
+    // Nested tuple
+    let nested = ((1.2, 'a'), (true, 1u32, 'b'), ());
+
+    // Accessing tuple
     let five_hundred = tup.0;
     println!("five hundred: {}", five_hundred);
+
+    println!("nested: {:?} {:?} {:?}", nested.0, nested.1, nested.2); 
+    println!("nested: {}", nested.0.0);
 
     // Data type: Array
     let arr: [u32; 5] = [1, 2, 3, 4, 5];
